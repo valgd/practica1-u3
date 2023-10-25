c1edd970a7531f73da700ca5585757256cf9cfda
LalokeraXD <lalokeraxxx@gmail.com>
2023-10-24 21:43:25 -0700

    Prueba 2

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -2,7 +2,7 @@ import { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
   appId: 'io.ionic.starter',
-  appName: 'Prueba',
+  appName: 'Práctica 1',
   webDir: 'www',
   server: {
     androidScheme: 'https'
--- src/app/services/carrito.service.ts
+++ src/app/services/carrito.service.ts
@@ -7,28 +7,25 @@ import { Product } from '../models/product.models';
 })
 export class CarritoService {
   private carrito: { product: Product, quantity: number }[] = [];
-  private carritoSubject: BehaviorSubject<{ product: Product, quantity: number }[]> = new BehaviorSubject(this.carrito);
 
-  constructor() {}
+  private carritoSubject = new BehaviorSubject<{ product: Product, quantity: number }[]>(this.carrito);
 
-  public agregarAlCarrito(producto: Product): void {
-    const productoEnCarrito = this.carrito.find((item) => item.product.id === producto.id);
-
-    if (productoEnCarrito) {
-      productoEnCarrito.quantity++;
+  agregarAlCarrito(producto: Product): void {
+    const productId = producto.id;
+    const item = this.carrito.find((el) => el.product.id === productId);
+    if (item) {
+      item.quantity++;
     } else {
       this.carrito.push({ product: producto, quantity: 1 });
     }
-
     this.carritoSubject.next(this.carrito);
   }
 
-  public obtenerCarrito(): Observable<{ product: Product, quantity: number }[]> {
+  obtenerCarrito(): Observable<{ product: Product, quantity: number }[]> {
     return this.carritoSubject.asObservable();
   }
 
-  public vaciarCarrito(): void {
-    this.carrito = [];
-    this.carritoSubject.next(this.carrito);
-  }
+  // Otros métodos para gestionar el carrito
+
+  // ...
 }
